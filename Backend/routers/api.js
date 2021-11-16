b175836db5d0f519d080734a2d01d70ef70c5512
Minnnnno <mervynphua9@gmail.com>
2021-11-16 17:19:27 +0800

    changed all the router names

--- Backend/routers/api.js
+++ Backend/routers/api.js
@@ -1,15 +1,13 @@
 const router = require("express").Router();
 const commentsRouter = require("./comments");
-const communitiesRouter = require("./communities");
-const likableRouter = require("./likable");
-const likesRouter = require("./likes");
-const postsRouter = require("./posts");
+const subredditRouter = require("./subreddits");
+const likesRouter = require("./post_votes");
+const postVotesRouter = require("./posts");
 const usersRouter = require("./comments");
 
 router.use("/comments", commentsRouter);
-router.use("/communities", communitiesRouter);
-router.use("/likable", likableRouter);
-router.use("/likes", likesRouter);
-router.use("/posts", postsRouter);
+router.use("/communities", subredditRouter);
+router.use("/postvotes", likesRouter);
+router.use("/posts", postVotesRouter);
 router.use("/users", usersRouter);
 module.exports = router;
