ebe22b219347b0d9b1aa2f2408118b04c1789020
Minnnnno <mervynphua9@gmail.com>
2021-11-16 21:17:12 +0800

    Added the backend for view comment

--- Backend/routers/comments.js
+++ Backend/routers/comments.js
@@ -1,36 +1,33 @@
 const router = require("express").Router();
 const connection = require("../database.js");
-var cors = require('cors');
+var cors = require("cors");
 router.use(cors());
 
-router.get('/viewcomment', function (req, res, next) {
-  const postid = req.body.postid;
+router.get("/viewcomment/:post_id", function (req, res, next) {
+  const postid = req.params.post_id;
   // const course = req.params.course;
-  const findusernameQuery = {
-      text: 'SELECT comment FROM comments WHERE postid = $1',
+  const findCommentQuery = {
+    text: "SELECT * FROM comments WHERE post_id = $1",
   };
-  console.log(findusernameQuery)
-  connection.query(findusernameQuery, (error, results) => {
-      if (error) {
-          console.log(error);
-          res
-              .status(500)
-              .json({
-                  Error: 'Something went wrong while finding comment'
-              });
+  console.log(postid);
+  connection.query(findCommentQuery, [postid], (error, results) => {
+    if (error) {
+      console.log(error);
+      res.status(500).json({
+        Error: "Something went wrong while finding comment",
+      });
+    } else {
+      if (results.rows.length === 0) {
+        res.status(404).json({
+          error: `Comment not found`,
+        });
       } else {
-          if (results.rows.length === 0) {
-              res.status(404).json({
-                  error: `Comment ${commentid} not found`
-              });
-          } else {
-              res.json({
-                  comment: results.rows
-              });
-          }
+        res.status(200).json({
+          comment: results.rows,
+        });
       }
+    }
   });
 });
-
 
 module.exports = router;
