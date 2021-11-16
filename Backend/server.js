d245a419bea74a3d4a712289e54769abf6d77ee6
Minnnnno <mervynphua9@gmail.com>
2021-11-16 17:03:51 +0800

    Added server.js and index.html

---
+++ Backend/server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const path = require("path");
+const ApiRouter = require("./routers/api");
+
+// The web server
+const app = express();
+
+// To handle body
+app.use(express.json());
+
+// Web Server
+app.use(express.static(path.join(__dirname, "public")));
+
+// APIs
+app.use("/api", ApiRouter);
+
+// TODO: 404 Handler
+
+// TODO: Error Handler
+
+// Listen to port
+app.listen(port, hostname, function () {
+  console.log(`BackEnd Server hosted at http://${hostname}:${port}`);
+});
