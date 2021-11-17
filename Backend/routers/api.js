dd80d4078a3cdfac1ded55c1f41dbe9bf94bc2f4
KannanMedhun <medhun.20@ichat.sp.edu.sg>
2021-11-17 15:02:42 +0800

    changes

--- Backend/routers/api.js
+++ Backend/routers/api.js
@@ -1,3 +1,7 @@
+const express = require('express')
+const cors = require('cors')
+
+
 const router = require("express").Router();
 const commentsRouter = require("./comments");
 const subredditRouter = require("./subreddits");
@@ -5,9 +9,20 @@ const likesRouter = require("./post_votes");
 const postVotesRouter = require("./post");
 const usersRouter = require("./users");
 
+const port = process.env.PORT
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+
 router.use("/comments", commentsRouter);
 router.use("/subreddits", subredditRouter);
 router.use("/postvotes", likesRouter);
 router.use("/post", postVotesRouter);
 router.use("/users", usersRouter);
 module.exports = router;
+
+app.listen(port, () => {
+    console.log(`App is listening on port ${port}`)
+  })
