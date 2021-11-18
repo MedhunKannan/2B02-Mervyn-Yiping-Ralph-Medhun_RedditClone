f06b13d282e91b13f93ecca5ef1c60a07988cd34
Yiping <syiping.20@ichat.sp.edu.sg>
2021-11-18 15:19:32 +0800

    delete and create comment

--- Backend/routers/comments.js
+++ Backend/routers/comments.js
@@ -3,9 +3,9 @@ const connection = require("../database.js");
 var cors = require("cors");
 router.use(cors());
 
-router.get("/viewcomment/:post_id", function (req, res, next) {
+// View Comments
+router.get("/viewComment/:post_id", function (req, res, next) {
   const postid = req.params.post_id;
-  // const course = req.params.course;
   const findCommentQuery = {
     text: "SELECT * FROM comments WHERE post_id = $1",
   };
@@ -28,6 +28,44 @@ router.get("/viewcomment/:post_id", function (req, res, next) {
       }
     }
   });
+});
+
+// Create Comment
+router.post('/createComment', (req, res) => {
+
+  var comment = req.body.comment;
+  const createCommentQuery = `
+    INSERT INTO comments(comment)
+    VALUES ($1);
+        `;
+
+  connection.query(createCommentQuery, [comment], (error, results) => {
+    if (error) {
+      console.log(error);
+      res.status(500).json({ error: 'Error while creating comment' });
+    } else {
+      console.log(results);
+      if (results.rowCount === 1) {
+        res.status(200).json({ message: 'Successfully created comment' });
+      } else {
+        res.status(404).json({ error: `Unable to create comment ${comment}` });
+      }
+    }
+  });
+});
+
+// Delete Comment
+router.delete("/deleteComment/:commentid", function (req, res, next) {
+  const commentid = req.params.commentid;
+  connection
+    .query(`DELETE FROM comments WHERE commentid = $1`, [commentid])
+    .then(function (result) {
+      res.status(200).json({ message: "Successfully deleted comment" });
+    })
+    .catch(function (error) {
+      console.log(error);
+      return next(error);
+    });
 });
 
 module.exports = router;
