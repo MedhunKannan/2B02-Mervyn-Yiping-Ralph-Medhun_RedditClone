d93b9047514bb2e5f2971d64f65fd01f521ae570
Minnnnno <mervynphua9@gmail.com>
2021-11-22 12:22:20 +0800

    post

--- Backend/routers/api.js
+++ Backend/routers/api.js
@@ -1,13 +1,12 @@
 const express = require('express')
 const cors = require('cors')
 
-
-const router = require("express").Router();
-const commentsRouter = require("./comments");
-const subredditRouter = require("./subreddits");
-const likesRouter = require("./post_votes");
-const postVotesRouter = require("./post");
-const usersRouter = require("./users");
+const router = require('express').Router()
+const commentsRouter = require('./comments')
+const subredditRouter = require('./subreddits')
+const likesRouter = require('./post_votes')
+const postVotesRouter = require('./post')
+const usersRouter = require('./users')
 
 // const port = process.env.PORT
 
@@ -16,12 +15,12 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
-router.use("/comments", commentsRouter);
-router.use("/subreddits", subredditRouter);
-router.use("/postvotes", likesRouter);
-router.use("/post", postVotesRouter);
-router.use("/users", usersRouter);
-module.exports = router;
+router.use('/comments', commentsRouter)
+router.use('/subreddits', subredditRouter)
+router.use('/post_votes', likesRouter)
+router.use('/post', postVotesRouter)
+router.use('/users', usersRouter)
+module.exports = router
 
 // app.listen(port, () => {
 //     console.log(`App is listening on port ${port}`)
--- Backend/routers/post_votes.js
+++ Backend/routers/post_votes.js
@@ -1,86 +1,85 @@
-const router = require("express").Router();
-const connection = require("../database.js");
-var cors = require('cors');
-router.use(cors());
+const router = require('express').Router()
+const connection = require('../database.js')
+var cors = require('cors')
+router.use(cors())
 
 // Upvoting
-router.put('/upVote/:postid', (req, res) => {
-
-    const postid = req.params.post_id;
-    const vote = req.body.vote;
-    const upVoteQuery = `
+router.put('/upVote/:post_id', (req, res) => {
+  const postid = req.params.post_id
+  //   const vote = req.body.vote_
+  const upVoteQuery = `
     UPDATE post_votes 
     SET vote_value = vote_value + 1
     WHERE post_id = $1
-          `;
+          `
 
-    connection.query(upVoteQuery, [vote, postid], (error, results) => {
-        if (error) {
-            console.log(error);
-            res.status(500).json({ error: 'Error while voting' });
-        } else {
-            console.log(results);
-            if (results.rowCount === 1) {
-                res.status(200).json({ message: 'Successfully voted' });
-            } else {
-                res.status(404).json({ error: `Unable to vote ${vote}` });
-            }
-        }
-    });
-});
+  connection.query(upVoteQuery, [postid], (error, results) => {
+    if (error) {
+      console.log(error)
+      res.status(500).json({ error: 'Error while voting' })
+    } else {
+      console.log(results)
+      if (results.rowCount === 1) {
+        res.status(200).json({ message: 'Successfully voted' })
+      } else {
+        console.log(error)
+        res.status(404).json({ error: `Unable to upvote` })
+      }
+    }
+  })
+})
 
 // Downvoting
 router.put('/downVote/:postid', (req, res) => {
-
-    const postid = req.params.post_id;
-    const vote = req.body.vote;
-    const downVoteQuery = `
+  const postid = req.params.post_id
+  const vote = req.body.vote
+  const downVoteQuery = `
     UPDATE post_votes 
     SET vote_value = vote_value - 1
     WHERE post_id = $1
-          `;
+          `
 
-    connection.query(downVoteQuery, [vote, postid], (error, results) => {
-        if (error) {
-            console.log(error);
-            res.status(500).json({ error: 'Error while voting' });
-        } else {
-            console.log(results);
-            if (results.rowCount === 1) {
-                res.status(200).json({ message: 'Successfully voted' });
-            } else {
-                res.status(404).json({ error: `Unable to vote ${vote}` });
-            }
-        }
-    });
-});
+  connection.query(downVoteQuery, [vote, postid], (error, results) => {
+    if (error) {
+      console.log(error)
+      res.status(500).json({ error: 'Error while voting' })
+    } else {
+      console.log(results)
+      if (results.rowCount === 1) {
+        res.status(200).json({ message: 'Successfully voted' })
+      } else {
+        res.status(404).json({ error: `Unable to vote ${vote}` })
+      }
+    }
+  })
+})
 
 // Get Vote
-router.get("/viewVotes/:post_id", function (req, res, next) {
-    const postid = req.params.post_id;
-    const findVoteQuery = ` 
+router.get('/viewVotes/:post_id', function (req, res, next) {
+  const postid = req.params.post_id
+  const findVoteQuery = ` 
     SELECT * FROM post_votes 
     WHERE post_id = $1 
-        `;
+        `
 
-    connection.query(findVoteQuery, [postid], (error, results) => {
-        if (error) {
-            console.log(error);
-            res.status(500).json({
-                Error: "Something went wrong while finding votes",
-            });
-        } else {
-            if (results.rows.length === 0) {
-                res.status(404).json({
-                    error: `Votes not found`,
-                });
-            } else {
-                res.status(200).json({
-                    vote: results.rows,
-                });
-            }
-        }
-    });
-});
+  connection.query(findVoteQuery, [postid], (error, results) => {
+    if (error) {
+      console.log(error)
+      res.status(500).json({
+        Error: 'Something went wrong while finding votes',
+      })
+    } else {
+      if (results.rows.length === 0) {
+        res.status(404).json({
+          error: `Votes not found`,
+        })
+      } else {
+        res.status(200).json({
+          vote: results.rows,
+        })
+      }
+    }
+  })
+})
 
-module.exports = router;
+module.exports = router
